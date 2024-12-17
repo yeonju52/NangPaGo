26241c1966aa4e1eb665e531dc2611e7225fba8c
CheolHyun Mun <zooxop@gmail.com>
2024-12-17 22:38:27 +0900

    Initialize Spring Boot project  (#1)
    * [Feat] add .gitignore
    
    * [Init] NangPaGo-be project
    
    * [Feat] 환경변수로 DB 연결 정보 가져오도록 application.yml 작성

---
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/NangPaGoApplication.java
@@ -0,0 +1,13 @@
+package com.mars.NangPaGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NangPaGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NangPaGoApplication.class, args);
+	}
+
+}
