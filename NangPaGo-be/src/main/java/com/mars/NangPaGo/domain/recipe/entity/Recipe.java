cd1c295d1fe50ebe1d0d5b470d22ac5081b6f6b8
johoon030 <142702766+hoonjo123@users.noreply.github.com>
2024-12-23 12:30:39 +0900

    [NPG-37] Recipe entity 작성 (#24)

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/entity/Recipe.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/entity/Recipe.java
@@ -1,26 +1,56 @@
 package com.mars.NangPaGo.domain.recipe.entity;
 
+import com.mars.NangPaGo.common.jpa.BaseEntity;
 import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
-public class Recipe {
+public class Recipe extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Recp_No;
+    @Column(name = "id")
+    private Long id;
 
-    private String Recp_Title;
+    @Column(name = "rcp_nm", nullable = false)
+    private String name;
 
-    private String Recp_Info;
+    @Column(name = "rcp_parts_dtls", columnDefinition = "TEXT")
+    private String ingredients;
 
-    private String Recp_ImgUrl;
+    @Column(name = "rcp_way2")
+    private String cookingMethod;
 
-    private String Recp_Ingredients;
+    @Column(name = "rcp_pat2")
+    private String category;
 
-    private String Recp_Category;
+    @Column(name = "info_eng")
+    private Integer calories;
 
+    @Column(name = "info_fat")
+    private Integer fat;
 
+    @Column(name = "info_car")
+    private Integer carbohydrates;
 
+    @Column(name = "info_pro")
+    private Integer protein;
 
+    @Column(name = "info_na")
+    private Integer sodium;
 
+    @Column(name = "hash_tag")
+    private String hashTag;
+
+    @Column(name = "att_file_no_main")
+    private String mainImage;
+
+    @Column(name = "att_file_no_mk")
+    private String stepImage;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "recipe")
+    private List<Manual> manuals;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "recipe")
+    private List<ManualImage> manualImages;
 }
