387eec0faf1a1eddd59c0169741a19fc78af53cb
yeonjuLee <yeonju509@gmail.com>
2024-12-26 14:36:58 +0900

    Elasticsearch Config 수정

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/config/ElasticsearchConfig.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/config/ElasticsearchConfig.java
@@ -1,4 +1,27 @@
 package com.mars.NangPaGo.config;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
 public class ElasticsearchConfig {
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        RestClient restClient = RestClient.builder(
+            new HttpHost("175.45.202.136", 9200, "http")
+        ).build();
+
+        RestClientTransport transport = new RestClientTransport(
+            restClient, new JacksonJsonpMapper());
+
+        return new ElasticsearchClient(transport);
+    }
 }
