76db0c78701f74f1e48b0fe7795929025df5468d
CheolHyun Mun <zooxop@gmail.com>
2025-01-08 12:09:12 +0900

    [NO-ISSUE] Service 레이어 테스트 유틸리티 구조 개선 (인증 관련) (#135)

--- NangPaGo-be/src/test/java/com/mars/NangPaGo/domain/recipe/service/RecipeLikeServiceTest.java
+++ NangPaGo-be/src/test/java/com/mars/NangPaGo/domain/recipe/service/RecipeLikeServiceTest.java
@@ -1,7 +1,9 @@
 package com.mars.NangPaGo.domain.recipe.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.mars.NangPaGo.common.exception.NPGException;
 import com.mars.NangPaGo.domain.recipe.dto.RecipeLikeResponseDto;
 import com.mars.NangPaGo.domain.recipe.entity.Recipe;
 import com.mars.NangPaGo.domain.recipe.entity.RecipeLike;
@@ -49,7 +51,7 @@ class RecipeLikeServiceTest extends IntegrationTestSupport {
     }
 
     @Transactional
-    @DisplayName("유저가 레시피에 좋아요를 클릭하여 RecipeLike 추가")
+    @DisplayName("레시피 좋아요를 클릭할 수 있다.")
     @Test
     void addRecipeLike() {
         // given
@@ -69,7 +71,8 @@ class RecipeLikeServiceTest extends IntegrationTestSupport {
         assertThat(recipeLikeResponseDto.recipeId()).isEqualTo(recipe.getId());
     }
 
-    @DisplayName("이미 좋아요를 누른 레시피에서 유저가 레시피에 좋아요를 클릭하여 RecipeLike 삭제")
+    @Transactional
+    @DisplayName("레시피 좋아요를 취소할 수 있다.")
     @Test
     void cancelRecipeLike() {
         // given
@@ -90,53 +93,102 @@ class RecipeLikeServiceTest extends IntegrationTestSupport {
             .isEqualTo(false);
     }
 
-//    @DisplayName("유저가 레시피에 좋아요를 클릭할때 SecurityContext의 email과 DB의 유저 email이 일치하지 않을 경우 예외처리")
-//    @Test
-//    void NotCorrectUserException() {
-//        // given
-//        setUp();
-//
-//        // mocking
-//        when(userRepository.findByEmail(anyString())).thenThrow(new NPGException(NOT_FOUND_USER));
-//
-//        // then
-//        assertThatThrownBy(() -> recipeLikeService.toggleLike(recipeId, email))
-//            .isInstanceOf(NPGException.class)
-//            .hasMessage("사용자를 찾을 수 없습니다.");
-//    }
-//
-//    @DisplayName("유저가 레시피에 좋아요를 클릭할때 레시피 ID를 못받는 경우 예외처리")
-//    @Test
-//    void NotFoundRecipeException() {
-//        // given
-//        setUp();
-//        RecipeLike recipeLike = RecipeLike.of(user, recipe);
-//
-//        // mocking
-//        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
-//        when(recipeRepository.findById(anyLong())).thenThrow(new NPGException(NOT_FOUND_RECIPE));
-//
-//        // when, then
-//        assertThatThrownBy(() -> recipeLikeService.toggleLike(recipeId, email))
-//            .isInstanceOf(NPGException.class)
-//            .hasMessage("레시피를 찾을 수 없습니다.");
-//    }
-//
-//    @DisplayName("이미 좋아요를 누른 레시피는 RecipeLike 엔티티를 조회할 수 있다.")
-//    @Test
-//    void isLikedByUser() {
-//        // given
-//        setUp();
-//        RecipeLike recipeLike = RecipeLike.of(user, recipe);
-//
-//        // mocking
-//        when(recipeLikeRepository.findByEmailAndRecipeId(anyString(), anyLong())).thenReturn(
-//            Optional.ofNullable(recipeLike));
-//
-//        // when
-//        boolean liked = recipeLikeService.isLikedByRecipe(recipeId, email);
-//
-//        // then
-//        assertThat(liked).isTrue();
-//    }
+    @DisplayName("좋아요를 클릭한 유저의 이메일을 찾을 수 없을 때 예외를 발생시킬 수 있다.")
+    @Test
+    void notFoundUserException() {
+        // given
+        User user = createUser("nonExistent@nangpago.com");
+        setAuthenticationAsUserWithToken(user.getEmail());
+        Recipe recipe = createRecipe("파스타");
+        recipeRepository.save(recipe);
+
+        Long recipeId = recipe.getId();
+        String email = user.getEmail();
+
+        // when & then
+        assertThatThrownBy(() -> recipeLikeService.toggleLike(recipeId, email))
+            .isInstanceOf(NPGException.class)
+            .hasMessage("사용자를 찾을 수 없습니다.");
+    }
+
+    @DisplayName("recipeId 로 레시피를 찾을 수 없을 때 예외를 발생시킬 수 있다.")
+    @Test
+    void notFoundRecipeException() {
+        // given
+        User user = createUser("example@nangpago.com");
+        userRepository.save(user);
+
+        String email = user.getEmail();
+
+        // when & then
+        assertThatThrownBy(() -> recipeLikeService.toggleLike(1L, email))
+            .isInstanceOf(NPGException.class)
+            .hasMessage("레시피를 찾을 수 없습니다.");
+    }
+
+    @DisplayName("좋아요를 누른 레시피는 좋아요 상태가 true 이다.")
+    @Test
+    void isLikedByUser() {
+        // given
+        User user = createUser("example@nangpago.com");
+        Recipe recipe = createRecipe("파스타");
+
+        userRepository.save(user);
+        recipeRepository.save(recipe);
+
+        RecipeLike recipeLike = RecipeLike.builder()
+            .user(user)
+            .recipe(recipe)
+            .build();
+
+        recipeLikeRepository.save(recipeLike);
+
+        // when
+        boolean isLiked = recipeLikeService.isLiked(recipe.getId(), user.getEmail());
+
+        // then
+        assertThat(isLiked).isTrue();
+    }
+
+    @DisplayName("좋아요를 누르지 않은 레시피는 좋아요 상태가 false 이다.")
+    @Test
+    void isNotLikedByUser() {
+        // given
+        User user = createUser("example@nangpago.com");
+        Recipe recipe = createRecipe("파스타");
+
+        userRepository.save(user);
+        recipeRepository.save(recipe);
+
+        // when
+        boolean isLiked = recipeLikeService.isLiked(recipe.getId(), user.getEmail());
+
+        // then
+        assertThat(isLiked).isFalse();
+    }
+
+    @Test
+    @DisplayName("다른 사용자가 좋아요한 레시피에 대해 현재 사용자의 좋아요 상태는 false 이다.")
+    void isLikedByDifferentUser() {
+        // given
+        User user1 = createUser("user1@nangpago.com");
+        User user2 = createUser("user2@nangpago.com");
+        Recipe recipe = createRecipe("파스타");
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+        recipeRepository.save(recipe);
+
+        RecipeLike recipeLike = RecipeLike.builder()
+            .user(user1)
+            .recipe(recipe)
+            .build();
+        recipeLikeRepository.save(recipeLike);
+
+        // when
+        boolean isLiked = recipeLikeService.isLiked(recipe.getId(), user2.getEmail());
+
+        // then
+        assertThat(isLiked).isFalse();
+    }
 }
