f98d17d30bcf1829eefedf0ad726f76f39f1f8a4
johoon030 <142702766+hoonjo123@users.noreply.github.com>
2025-01-09 11:01:32 +0900

    [NPG-115] 커뮤니티 기본 사진 등록 (#150)

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/community/dto/CommunityResponseDto.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/community/dto/CommunityResponseDto.java
@@ -18,18 +18,25 @@ public record CommunityResponseDto(
     LocalDateTime createdAt,
     LocalDateTime updatedAt
 ) {
+    public static final String DEFAULT_IMAGE_URL =
+        "https://storage.googleapis.com/nangpago-9d371.firebasestorage.app/dc137676-6240-4920-97d3-727c4b7d6d8d_360_F_517535712_q7f9QC9X6TQxWi6xYZZbMmw5cnLMr279.jpg";
+
     public static CommunityResponseDto of(Community community, String email) {
         return CommunityResponseDto.builder()
             .id(community.getId())
             .title(community.getTitle())
             .content(community.getContent())
-            .imageUrl(community.getImageUrl())
+            .imageUrl(getImageUrlOrDefault(community.getImageUrl()))
             .email(maskEmail(community.getUser().getEmail()))
             .isOwnedByUser(community.getUser().getEmail().equals(email))
             .isPublic(community.isPublic())
             .createdAt(community.getCreatedAt())
             .updatedAt(community.getUpdatedAt())
             .build();
+    }
+
+    private static String getImageUrlOrDefault(String imageUrl) {
+        return (imageUrl == null || imageUrl.isBlank()) ? DEFAULT_IMAGE_URL : imageUrl;
     }
 
     private static String maskEmail(String email) {
