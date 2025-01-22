f392951086f28a2c765f2b1205e2348ef51b27a4
JTH-00 <100738494+JTH-00@users.noreply.github.com>
2025-01-22 09:48:23 +0900

    [NPG-167] 관리자 패스워드 암호화 추가 (#232)

--- NangPaGo-api/NangPaGo-admin/src/main/java/com/mars/admin/config/SecurityConfig.java
+++ NangPaGo-api/NangPaGo-admin/src/main/java/com/mars/admin/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
@@ -40,6 +42,11 @@ public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
     private final AdminSuccessHandler adminSuccessHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
