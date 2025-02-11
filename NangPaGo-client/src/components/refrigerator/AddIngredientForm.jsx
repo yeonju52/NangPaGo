de749b9bf8008ee4d316ade2493d97f4e0ceecf9
JTH-00 <100738494+JTH-00@users.noreply.github.com>
2025-02-11 14:17:37 +0900

    [NPG-254] 내 냉장고 재료 검색창 UI 개선 (#344)

--- NangPaGo-client/src/components/refrigerator/AddIngredientForm.jsx
+++ NangPaGo-client/src/components/refrigerator/AddIngredientForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { BiSearch } from 'react-icons/bi';
 
 const AddIngredientForm = ({ onAdd }) => {
   const [ingredientName, setIngredientName] = useState('');
@@ -20,16 +21,23 @@ const AddIngredientForm = ({ onAdd }) => {
     setIngredientName('');
   };
 
+  const getIcon = () => {
+    return (
+      <BiSearch className="absolute right-3 text-text-400 text-2xl cursor-pointer" />
+    );
+  };
+
   return (
-    <form onSubmit={handleSubmit} className="flex items-center gap-2">
-      <input
-        type="text"
-        value={ingredientName}
-        onChange={handleChange}
+    <form onSubmit={handleSubmit}>
+      <div
+        className="relative flex items-center border border-primary rounded-md bg-white cursor-pointer"
         onClick={handleInputClick}
-        placeholder="재료 이름 입력"
-        className="border border-primary p-2 rounded-md flex-grow text-text-400"
-      />
+      >
+        <div className={`w-full px-4 py-2 text-text-400`}>
+          {'재료 검색...'}
+        </div>
+        {getIcon()}
+      </div>
     </form>
   );
 };
